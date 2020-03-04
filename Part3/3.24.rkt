06178d91aa548bd2bea75f898df577d30960e586
lsgsk <lsgsk@rambler.ru>
2020-03-04 06:55:05 +0300

    wip 3.24

---
+++ Part3/3.24.rkt
@@ -0,0 +1,31 @@
+#lang planet neil/sicp
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (lookup key table)
+  (let ((record  (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table)))))
+  'ok)
+
+(define (make-table)
+  (list '*table*))
+
+(define table (make-table))
+(insert! 1 'a table)
+(insert! 3 'b table)
+(insert! 2 'c table)
+(insert! 4 'd table)
+
+table
